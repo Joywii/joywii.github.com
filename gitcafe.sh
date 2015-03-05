538870165007d52bd1899d0a262b91e028806c34
Joywii <kingzwt@126.com>
2015-03-05 18:42:42 +0800

    fix gitcafe.sh

--- gitcafe.sh
+++ gitcafe.sh
@@ -5,5 +5,5 @@ cd _deploy
 git remote add gitcafe git@gitcafe.com:joywii/joywii.git >> /dev/null 2>&1
 # 提交博客内容
 echo "### Pushing to GitCafe..."
-git push -u gitcafe master:gitcafe-pages
+git push -u -f gitcafe master:gitcafe-pages
 echo "### Done"%
